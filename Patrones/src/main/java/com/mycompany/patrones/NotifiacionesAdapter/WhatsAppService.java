7e0227549c18c4089fdaea82b821de86d3be2fe1
OwuenYagual <118647582+OwuenYagual@users.noreply.github.com>
2024-07-10 16:14:23 -0500

    correciones de adapter

--- Patrones/src/main/java/com/mycompany/patrones/NotifiacionesAdapter/WhatsAppService.java
+++ Patrones/src/main/java/com/mycompany/patrones/NotifiacionesAdapter/WhatsAppService.java
@@ -3,7 +3,7 @@ package com.mycompany.patrones.NotifiacionesAdapter;
 class WhatsAppService {
 
     void sendMessage(String message) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        System.out.println(message);
     }
     
 }
